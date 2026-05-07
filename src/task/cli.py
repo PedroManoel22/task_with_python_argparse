72142d5b641dd14d5b019938b002b5b46b2d02f8
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-07 14:48:28 -0300

    add subparser for task creation

--- src/task/cli.py
+++ src/task/cli.py
@@ -1,10 +1,66 @@
 import argparse
+import textwrap
+
+import rich_argparse
 
 from task.settings import clear
+from task.validation import validate_str
 
 
 def build_parser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        prog="task",
+        description=textwrap.dedent(
+            """
+    Task Manager helps you organize your tasks directly from the terminal.
+
+    You can create, search, list, and delete tasks with ease - all from
+    your CLI. No need for web apps, mouse clicks or distractions.
+    Just productivity.
+
+"""
+        ),
+        epilog=textwrap.dedent("""
+    This will be shown below all arguments and can be used to add
+    copyright or other complex examples."""),
+        formatter_class=rich_argparse.RawDescriptionRichHelpFormatter,
+        # formata a mensagem do jeito que escrevemos
+    )
+
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    create_parser = subparsers.add_parser(
+        "create",
+        aliases=["new", "add"],
+        description=textwrap.dedent("""
+    Use this command to create a new task quickly and efficiently.
+
+    Provide a title, optional tags, priority, and mark it as done if needed.
+    Whether you're planning your day or dumping ideas into the terminal,
+    this is your entry point."""),
+        epilog=textwrap.dedent("""Examples:
+
+      task create -t "Buy groceries"
+      task create -t "Study argparse" --tag python --tag cli --priority high
+      task create -t "Walk the dog" --done
+
+    You can also combine options freely to match your workflow.
+    Tags help with filtering later. Priorities can be: low, medium, high."""),
+        formatter_class=rich_argparse.RawDescriptionRichHelpFormatter,
+    )
+
+    create_parser.add_argument(
+        "-t",
+        "--task",
+        type=validate_str,
+        help="Describies your task",
+        required=True,
+        metavar="Task",
+    )
+
+    create_parser.add_argument(
+        "--done", help="Mrks a task as complete", action=argparse.BooleanOptionalAction
+    )
 
     return parser
 
